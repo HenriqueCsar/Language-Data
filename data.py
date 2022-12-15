514868ae42faac70db829c8f611531be211a9e8e
HenriqueCsar <henrique.fibonacci@hotmail.com>
2022-12-15 07:56:12 -0300

    Adjustment in IlegalCharError

--- data.py
+++ data.py
@@ -16,15 +16,17 @@ DIGITS = '0987654321'
 class Error:
     def __init__(self, error_name, details):
         self.error_name = error_name
-        self.details == details
+        self.details = details
 
     def as_string(self):
         result = f'{self.error_name}: {self.details}'
         return result
 
-class IlegalCharError:
+class IlegalCharError(Error):
     def __init__(self, details):
         super().__init__('Illegal Character', details)
+
+
         
 ######
 ##
