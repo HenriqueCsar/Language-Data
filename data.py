ad72303bc04d9c929b40ac47846540736fb47f27
HenriqueCsar <henrique.fibonacci@hotmail.com>
2022-12-18 19:41:04 -0300

    Update Code Language Data V20221218

--- data.py
+++ data.py
@@ -230,10 +230,34 @@ class UnaryOpNode:
 
     def __repr__(self):
         return f'({self.op_tok}, {self.node})'
+
 #######
-##  PARSER
+##  PARSER Result
 #######
 
+class ParseResult:
+    def __init__(self):
+        self.error = None
+        self.node = None
+
+    def register(self, res):
+        if isinstance(res, ParseResult):
+            if res.error: self.error = res.error
+            return res.node
+        
+        return res
+
+    def success(self, node):
+        self.node = node
+        return self
+    
+    def failure(self, error):
+        self.error = error
+        return self
+
+########
+#PARSE
+########
 
 
 
