cae43c56afe0eaa73c1be87d03cc0266876b254d
HenriqueCsar <henrique.fibonacci@hotmail.com>
2022-12-18 19:33:44 -0300

    Update Code Language Data V20221218

--- data.py
+++ data.py
@@ -20,10 +20,32 @@ class Error:
         self.error_name = error_name
         self.details = details
 
+    def error_string_with_arrows(text, pos_start, pos_end):
+        result = ''
+        idx_start = max(text.rfind('\n', 0, pos_start.idx), 0)
+        idx_end = text.find('\n', idx_start  + 1)
+        if idx_end < 0: idx_end = len(text)
+
+
+        line_count = pos_end.ln - pos_start.ln + 1
+        for i in range(line_count):
+            line = text[idx_start:idx_end]
+            col_start = pos_start.col if i == 0 else 0
+            col_end = pos_end.col if i == line_count - 1 else len(line)
+
+            result+=line + '\n'
+            result+= ' ' * col_start + '^' * (col_end - col_start)
+
+            idx_start = idx_end
+            idx_end = text.find('\n', idx_start + 1)
+            if idx_end < 0: idx_end = len(text)
+
+        return result.replace('\t', '')
+
     def as_string(self):
         result  = f'{self.error_name}: {self.details}'
         result += f'File: {self.pos_start.fn}, line {self.pos_start.ln + 1}'
-
+        result += '\n\n' + Error.error_string_with_arrows(self.pos_start.ftxt, self.pos_start, self.pos_end)
         return result
 
 class IlegalCharError(Error):
@@ -43,18 +65,21 @@ class Position:
         self.fn = fn
         self.ftxt = ftxt
 
-    def advance(self, current_char):
+    def advance(self, current_char=None):
         self.idx += 1
         self.col +=1
 
         if current_char == '\n':
             self.ln += 1
             self.col = 0
+
         return self
 
     def copy(self):
         return Position(self.idx, self.ln, self.col, self.fn, self.ftxt)
-        
+
+
+
 ######
 ##
 ## Type of the Tokens
@@ -69,16 +94,24 @@ TT_MUL      = 'MUL'
 TT_DIV      = 'DIV'
 TT_LPAREN   = 'LPAREN'
 TT_RPAREN   = 'RPAREN'
-
+TT_EOF      = 'EOF'
 
 class Token:
-    def __init__(self, type_, value=None):
+    def __init__(self, type_, value=None, pos_start=None, pos_end=None):
         self.type = type_
         self.value = value
 
+        if pos_start:
+            self.pos_start = pos_start.copy()
+            self.pos_end = pos_start.copy()
+            self.pos_end.advance()
+            
+        if pos_end:
+            self.pos_end = pos_end
+
     def __repr__(self):
         if self.value: 
-            return f'{self.type}:{str(self.value)}'
+            return f'{self.type}:{self.value}'
         return f'{self.type}'
     
 
@@ -107,6 +140,7 @@ class Lexer:
             # print('Pass here')
             if self.current_char in ' \t':
                 self.advance()
+
             elif self.current_char in DIGITS:
                 tokens.append(self.make_number())
                 
@@ -136,12 +170,13 @@ class Lexer:
                 self.advance()
                 return [], IlegalCharError(pos_start,self.pos,"'" + char + "'")
             
+        tokens.append(Token(TT_EOF, pos_start=self.pos))
         return tokens, None
 
     def make_number(self):
         num_str = ''
         dot_count = 0
-
+        pos_start = self.pos.copy()
 
         while self.current_char != None and self.current_char in DIGITS + '.':
             if(self.current_char == '.'):
@@ -167,6 +202,9 @@ class NumberNode:
     def __init__(self, tok):
         self.tok = tok
 
+        self.pos_start = self.tok.pos_start
+        self.pos_end = self.tok.pos_end
+
     def __repr__(self):
         return f'{self.tok}'
 
@@ -176,9 +214,22 @@ class BinOpNode:
         self.op_tok = op_tok
         self.right_node = right_node
 
+        self.pos_start = self.left_node.pos_start
+        self.pos_end = self.right_node.pos_end
+
     def __repr__(self):
         return f'({self.left_node}, {self.op_tok}, {self.right_node})'
 
+class UnaryOpNode:
+    def __init__(self, op_tok, node):
+        self.op_tok = op_tok
+        self.node = node
+
+        self.pos_start = self.op_tok.pos_start
+        self.pos_end = node.pos_end
+
+    def __repr__(self):
+        return f'({self.op_tok}, {self.node})'
 #######
 ##  PARSER
 #######
