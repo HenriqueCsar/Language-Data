ca46c62db443bbd85a5e5501a627ed662c0b97aa
HenriqueCsar <henrique.fibonacci@hotmail.com>
2022-12-18 23:39:51 -0300

    Update Code Language Data V20221218

--- data.py
+++ data.py
@@ -426,7 +426,10 @@ class Interpreter:
 
         if node.op_tok.type == TT_PLUS:
             result, error = left.added_to(right)
-
+        elif node.op_tok_type == TT_MINUS:
+            result, error = left.subbed_by(right)
+        elif node.op_tok_type == TT_MUL:
+            result, error = left.multed_by(right)
 
 
 
