e250c5f841b541b1f56bbac4887690176a329777
HenriqueCsar <henrique.fibonacci@hotmail.com>
2022-12-19 00:04:02 -0300

    Update Code Language Data V20221219

--- data.py
+++ data.py
@@ -424,6 +424,7 @@ class Interpreter:
     def visit(self, node, context):
         method_name = f'visit_{type(node).__name__}'
         method = getattr(self, method_name, self.no_visit_method)
+        return method(node,context)
 
     def no_visit_method(self, node, context):
         raise Exception(f'No visit_{type(node).__name__} method defined')
