4627922028c3fe7335d719c6fd980e15a57fc4f9
HenriqueCsar <henrique.fibonacci@hotmail.com>
2022-12-18 22:11:19 -0300

    Update Code Language Data V20221218

--- data.py
+++ data.py
@@ -56,6 +56,11 @@ class InvalidSyntaxError(Error):
 	def __init__(self, pos_start, pos_end, details=''):
 		super().__init__(pos_start, pos_end, 'Invalid Syntax', details)
 
+class RTError(Error):
+    def __init__(self, pos_start, pos_end, details, context):
+        super().__init__(pos_start, pos_end, 'Invalid Syntax', details)
+        self.context = context
+
 ######
 ##Position
 ######
