14b2b38dddf9b24fc22531b00567137cffb10f38
HenriqueCsar <henrique.fibonacci@hotmail.com>
2022-12-18 22:31:07 -0300

    Update Code Language Data V20221218

--- data.py
+++ data.py
@@ -393,6 +393,9 @@ class Number:
 #CONTEXT
 ########
 
+class Context:
+    def __init__(self, display_name, parent=None, parent_entry_pos=None):
+        self.display_name = display_name
 
 
 ########
